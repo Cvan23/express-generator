eaafc81083cfdd2af7b0dfd938d80e7691fc2b89
Chris Van Huffe <cvanhuffel@outlook.com>
2020-11-06 14:29:07 -0800

    FBook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+  'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientId: '2858452751040686',
+    clientSecret: '0919d6d5696571535aa880016e551b80'
+}
 }
